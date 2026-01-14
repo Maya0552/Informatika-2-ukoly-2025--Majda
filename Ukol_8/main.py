a60cf07c5e1c0b5d3b4a8935c18791d0dd467136
Maya0552 <magdalena.cetlova@unob.cz>
2026-01-14 20:50:08 +0000

    Ukol 8 - Magdaléna Cetlová

--- Ukol_8/main.py
+++ Ukol_8/main.py
@@ -6,6 +6,9 @@ def read_logs(file_path):
     Ošetřete FileNotFoundError.
     """
     try:
+        with open(file_path, "r", encoding="utf-8") as f:
+            for line in f:
+                yield line.strip()
         # TODO: Otevřít soubor a yieldovat řádky
         pass
     except FileNotFoundError:
@@ -21,12 +24,20 @@ def process_line(line):
     """
     # TODO: Definovat regex pattern
     # pattern = r"..."
+    pattern = r"^\[(.*?)\]\s+(\w+):\s+(.*?)\s+-\s+User:\s+([^\s]+)$"
     
     # TODO: Použít re.search nebo re.match
-    # match = ...
+    match = re.match(pattern, line)
     
     # TODO: Pokud match, vrátit dict, jinak None
-    pass
+    if match:
+        return {
+            'timestamp': match.group(1),
+            'level': match.group(2),
+            'message': match.group(3),
+            'email': match.group(4)
+        }
+    return None
 
 def analyze_logs(input_file, output_file):
     """
@@ -39,6 +50,12 @@ def analyze_logs(input_file, output_file):
         # TODO: Zpracovat řádek přes process_line
         # TODO: Pokud je level == 'ERROR', zapsat do souboru
         # pass
+    with open(output_file, 'w', encoding='utf-8') as f_out:
+        for line in read_logs(input_file):
+            result = process_line(line)  
+            if result and result['level'] == 'ERROR':
+                f_out.write(line + '\n')
+                count += 1  
     
     print(f"Zpracování dokončeno. Nalezeno {count} chyb.")
 
